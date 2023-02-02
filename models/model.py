4f361a20f818bbc37b81e1b5e839d424dae584ca
taylanates <taylan.ates@data-boss.com.tr>
2023-02-02 21:13:15 +0300

    Add ScalableCSPResBlock

--- models/model.py
+++ models/model.py
@@ -40,18 +40,73 @@ class TyHead(nn.Module):
         pass
     
 
-class CSPResSPPBlock(nn.Module):
+class ScalableCSPResBlock(nn.Module):
 
-    def __init__(self) -> None:
-        super(CSPResSPPBlock, self).__init__()
+    def __init__(self, in_ch=512, num_basic_layers=1) -> None:
+        super(ScalableCSPResBlock, self).__init__()
 
-    pass
-
+        
+        self.conv1 = nn.Conv2d(in_channels=in_ch, out_channels=in_ch*2, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(num_features=in_ch*2)
+        self.conv2 = nn.Conv2d(in_channels=in_ch*2, out_channels=in_ch*2, kernel_size=1, stride=1, padding=0)
+        self.bn2 = nn.BatchNorm2d(num_features=in_ch*2)
+        basic_layers = []
+        
+        for _ in range(num_basic_layers):
+            basic_layers.append(BasicBlock(in_ch, in_ch))
+            
+        self.basic_layers = nn.Sequential(*basic_layers)
+        self.transition = nn.Conv2d(in_ch*2, in_ch, kernel_size=1, stride=1, padding=0)
+        self.act = nn.Mish()
 
     def forward(self, x):
+        x = self.act(self.bn1(self.conv1(x)))
+        x = self.act(self.bn2(self.conv2(x)))
+        
+        xs, xb = x.split(x.shape[1] // 2, dim=1)
+        xb = self.basic_layers(xb)
+        
+        out = self.transition(torch.cat([xs, xb], dim=1))
 
-        pass
+        return out
 
+
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride = 1, downsample = False):
+        super(BasicBlock, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=(3, 3), stride=stride, padding=(1, 1), bias=False)
+        self.bn1 = nn.BatchNorm2d(out_channels, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        self.act = nn.Mish()
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=(3, 3), stride=(1,1), padding=(1, 1), bias=False)
+        self.bn2 = nn.BatchNorm2d(out_channels, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+        
+        if downsample:
+            self.downsample = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=(1, 1), stride=(2, 2), bias=False),
+                nn.BatchNorm2d(out_channels, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True)
+                )
+        else:
+            self.downsample = None
+        self.out_channels = out_channels
+    
+    
+    def forward(self, x):
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.act(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+        
+        if self.downsample is not None:
+            x = self.downsample(x)
+            
+        out = x + out
+        out = self.act(out)
+        
+        return out
 
 
 
