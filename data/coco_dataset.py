5ad2ff0ab5bd6db3c04ba95402e740cb2e738e14
taylan.ates <taylan.ates417311@gmail.com>
2023-02-08 23:22:45 +0300

    Add dataset class

---
+++ data/coco_dataset.py
@@ -0,0 +1,18 @@
+from torch.utils.data import Dataset
+
+
+class CustomDataset(Dataset):
+
+    def __init__(self) -> None:
+        super(CustomDataset, self).__init__()
+        
+        pass
+    
+    def __len__(self):
+        
+        pass
+    
+    
+    def __getitem__(self, index):
+        
+        pass
