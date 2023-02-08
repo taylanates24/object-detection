c883feeb411f44f6ca758fbce0e0182d0aeaa151
taylan.ates <taylan.ates417311@gmail.com>
2023-02-08 22:50:05 +0300

    Add annotations to annotation file

--- data/yolo_to_coco.py
+++ data/yolo_to_coco.py
@@ -7,9 +7,9 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('--yolo_ann', type=str, 
+    parser.add_argument('--yolo_annotationsn', type=str, 
                         default='/workspaces/object-detection/datasets/coco128/labels/train2017', 
-                        help='Yolo annotations path')
+                        help='Yolo annotationsnotations path')
     parser.add_argument('--yolo_img', type=str, 
                         default='/workspaces/object-detection/datasets/coco128/images/train2017', 
                         help='Yolo categories path')
@@ -23,15 +23,15 @@ if __name__ == '__main__':
     coco_names = open(args.coco_names).read()
     coco_names = coco_names.split('\n')[:-1]
     
-    coco_annotations = {
+    coco_annotationsnotations = {
         "info": {},
         'images': [],
-        'annotations': [],
+        'annotationsnotations': [],
         'categories': []
     }
     
     for i, category in enumerate(coco_names):
-        coco_annotations['categories'].append(
+        coco_annotationsnotations['categories'].append(
             {
                 'id': i + 1,
                 'name': category
@@ -39,9 +39,9 @@ if __name__ == '__main__':
         )
         
 
-for an in sorted(os.listdir(args.yolo_ann)):
-    ann_id = 1
-    img_id = an.split('.')[0]
+for annotations in sorted(os.listdir(args.yolo_annotationsn)):
+    annotationsn_id = 1
+    img_id = annotations.split('.')[0]
     img_name = img_id + '.jpg'
     img = cv2.imread(os.path.join(args.yolo_img, img_name))
     
@@ -49,7 +49,7 @@ for an in sorted(os.listdir(args.yolo_ann)):
         continue
     h, w, c = img.shape
     
-    coco_annotations['images'].append(
+    coco_annotationsnotations['images'].append(
         {
             'file_name': img_name,
             'height': h,
@@ -57,4 +57,22 @@ for an in sorted(os.listdir(args.yolo_ann)):
             'id': int(img_id)
             }
         )
+    
+    annotations = open(os.path.join(args.yolo_annotationsn, annotations)).read().split('\n')
+    
+    for bbox in annotations[:-1]:
         
+        values = bbox.split(' ')
+        
+        coco_annotationsnotations['annotationsnotations'].append(
+            {
+                'image_id': int(img_id),
+                'bbox': [(float(values[1]) - (float(values[3]) / 2)) * w, 
+                            (float(values[2]) - (float(values[4]) / 2)) * h,
+                            float(values[3]) * w, 
+                            float(values[4]) * h
+                ],
+                'category_id': int(values[0]),
+                'id': annotationsn_id
+            }
+        )
