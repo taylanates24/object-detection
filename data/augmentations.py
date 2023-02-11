51655f355079c73d4edcaaff1ec71a018fc45f1c
taylanates24 <taylan.ates417311@gamil.com>
2023-02-12 01:58:41 +0300

    Add SaturationShift augmentation definition

--- data/augmentations.py
+++ data/augmentations.py
@@ -18,5 +18,19 @@ class BrightnessShift:
     
     
     def __call__(self, img, bboxes):
-        
+        #aug = iaa.AddToBrightness((-30, 30))
         pass
+    
+    
+    
+class SaturationShift:
+    
+    def __init__(self) -> None:
+        pass
+    
+    
+    def __call__(self, img, bboxes):
+        #aug = iaa.AddToSaturation((-50, 50))
+        pass
+    
+    
