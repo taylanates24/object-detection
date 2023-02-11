d095279f66599e13d4a962025a66307990a039d5
taylanates24 <taylan.ates417311@gamil.com>
2023-02-12 02:15:18 +0300

    Add MotionBlur definition

--- data/augmentations.py
+++ data/augmentations.py
@@ -51,7 +51,33 @@ class AddGrayscale:
     
     
     def __call__(self, img, bboxes):
-        #aug = iaa.AddToHue((-50, 50))
+        #aug = iaa.Grayscale(alpha=(0.0, 1.0))
         pass
     
     
+class ChangeColorTemperature:
+    
+    def __init__(self) -> None:
+        pass
+    
+    
+    def __call__(self, img, bboxes):
+        #aug = iaa.ChangeColorTemperature((1100, 10000))
+        pass
+    
+
+
+class MotionBlur:
+    
+    def __init__(self) -> None:
+        pass
+    
+    
+    def __call__(self, img, bboxes):
+        #aug = iaa.MotionBlur(k=15)
+        pass
+
+'''     bbox_aug = []
+        for bbox in bboxes:
+            bbox_aug.append(BoundingBox(x1=bbox[0], y1=bbox[1], x2=bbox[0]+bbox[2], y2=bbox[1]+bbox[3]))
+        bbs = BoundingBoxesOnImage(bbox_aug, shape=(img_heigth, img_width, 3))'''
