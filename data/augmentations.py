78c05b4b69759618112eb1579365f74dcd4e0fb8
taylanates24 <taylan.ates417311@gamil.com>
2023-02-12 01:56:39 +0300

    Add BrightnessShift augmentation

---
+++ data/augmentations.py
@@ -0,0 +1,22 @@
+import cv2
+import imgaug.augmenters as aug
+
+class HorizontalFlip:
+    
+    def __init__(self) -> None:
+        pass
+    
+    
+    def __call__(self, img, bboxes):
+        
+        pass
+    
+class BrightnessShift:
+    
+    def __init__(self) -> None:
+        pass
+    
+    
+    def __call__(self, img, bboxes):
+        
+        pass
