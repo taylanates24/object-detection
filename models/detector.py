e0d4f6952c29845bae915ebd8f6b08a4339e7508
taylanates24 <taylan.ates417311@gmail.com>
2023-02-23 11:04:52 +0300

    Update validation epoch end

--- models/detector.py
+++ models/detector.py
@@ -11,6 +11,7 @@ class Detector(pl.LightningModule):
         self.model = model
         self.scheduler = scheduler
         self.optimizer = optimizer
+        self.best_val_loss = float('inf')
 
         if loss is None:
             self.loss = FocalLoss()
@@ -55,23 +56,34 @@ class Detector(pl.LightningModule):
 
         output = self.forward(images, labels)
         cls_loss, reg_loss = self.loss(classification, regression, anchors, annotations)
+        total_loss = cls_loss + reg_loss
 
-        return {'cls_loss': cls_loss, 'reg_loss': reg_loss}
+        return {'loss':total_loss,  'cls_loss': cls_loss, 'reg_loss': reg_loss}
 
     def validation_epoch_end(self, outputs):
 
         cls_losses = [x['cls_loss'] for x in outputs]
         reg_losses = [x['reg_loss'] for x in outputs]
+        total_losses = [x['loss'] for x in outputs]
         
         avg_val_cls_loss = sum(cls_losses) / len(cls_losses)
         avg_val_reg_loss = sum(reg_losses) / len(reg_losses)
-        total losses
-        if
+        avg_val_loss = sum(total_losses) / len(total_losses)
+        
+        if  self.best_val_loss > avg_val_loss:
+            self.best_val_loss = avg_val_loss
+
+            torch.save({
+                'epoch': self.current_epoch,
+                'model_state_dict': self.model.state_dict(),
+                'optimizer_state_dict': self.optimizer.state_dict(),
+                'loss': self.loss,
+                }, './best.ckpt')
 
         self.log('validation cls_loss', avg_val_cls_loss)
         self.log('validation reg_loss', avg_val_reg_loss)
-
-
+        self.log('validation total_loss', avg_val_loss)
+        
 
     def test_step(self, batch, batch_idx):
 
