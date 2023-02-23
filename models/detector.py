5114c5511eaf8f5ac4fc2efc5f34f323d87bae45
taylanates24 <taylan.ates417311@gmail.com>
2023-02-23 11:00:17 +0300

    Update training steps and add validation epoch end

--- models/detector.py
+++ models/detector.py
@@ -28,21 +28,25 @@ class Detector(pl.LightningModule):
 
         output = self.forward(images, labels)
         cls_loss, reg_loss = self.loss(classification, regression, anchors, annotations)
+        total_loss = cls_loss + reg_loss
 
         self.log('learning rate', self.scheduler.get_lr()[0])
 
-        return {'cls_loss': cls_loss, 'reg_loss': reg_loss}
+        return {'loss': total_loss, 'cls_loss': cls_loss, 'reg_loss': reg_loss}
 
     def training_epoch_end(self, outputs):
         
         cls_losses = [x['cls_loss'] for x in outputs]
         reg_losses = [x['reg_loss'] for x in outputs]
+        total_losses = [x['loss'] for x in outputs]
         
         avg_train_cls_loss = sum(cls_losses) / len(cls_losses)
         avg_train_reg_loss = sum(reg_losses) / len(reg_losses)
+        avg_train_loss = sum(total_losses) / len(total_losses)
 
         self.log('train cls_loss', avg_train_cls_loss)
         self.log('train reg_loss', avg_train_reg_loss)
+        self.log('train total_loss', avg_train_loss)
 
     def validation_step(self, val_batch, batch_idx):
 
@@ -56,7 +60,18 @@ class Detector(pl.LightningModule):
 
     def validation_epoch_end(self, outputs):
 
-        pass
+        cls_losses = [x['cls_loss'] for x in outputs]
+        reg_losses = [x['reg_loss'] for x in outputs]
+        
+        avg_val_cls_loss = sum(cls_losses) / len(cls_losses)
+        avg_val_reg_loss = sum(reg_losses) / len(reg_losses)
+        total losses
+        if
+
+        self.log('validation cls_loss', avg_val_cls_loss)
+        self.log('validation reg_loss', avg_val_reg_loss)
+
+
 
     def test_step(self, batch, batch_idx):
 
