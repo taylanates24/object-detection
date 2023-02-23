7e6bccf22788496cd54600cdd33f807cb1bea23f
taylanates24 <taylan.ates417311@gmail.com>
2023-02-23 10:53:07 +0300

    Add training_epoch_end

--- models/detector.py
+++ models/detector.py
@@ -34,8 +34,15 @@ class Detector(pl.LightningModule):
         return {'cls_loss': cls_loss, 'reg_loss': reg_loss}
 
     def training_epoch_end(self, outputs):
+        
+        cls_losses = [x['cls_loss'] for x in outputs]
+        reg_losses = [x['reg_loss'] for x in outputs]
+        
+        avg_train_cls_loss = sum(cls_losses) / len(cls_losses)
+        avg_train_reg_loss = sum(reg_losses) / len(reg_losses)
 
-        pass
+        self.log('train cls_loss', avg_train_cls_loss)
+        self.log('train reg_loss', avg_train_reg_loss)
 
     def validation_step(self, val_batch, batch_idx):
 
